b50528782c25a424a2e59235581a0c54683e53e6
GdoongMathew <gdoongmathew@gmail.com>
2024-08-04 17:18:23 +0800

    refactor: reimplement the Vec3d precision from f32 to f64.

--- src/vec3d.rs
+++ src/vec3d.rs
@@ -4,7 +4,7 @@ use std::ops::{Add, AddAssign, Sub, SubAssign, Mul, MulAssign, Div, DivAssign, N
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Vec3d {
-    vector: (f32, f32, f32),
+    vector: (f64, f64, f64),
 }
 
 /// Implementation of ``Vec3d``
@@ -25,17 +25,17 @@ pub struct Vec3d {
 /// assert_eq!(vec.z(), 3.0);
 /// ```
 impl Vec3d {
-    pub fn new(x: f32, y: f32, z: f32) -> Self {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
         Self {
             vector: (x, y, z)
         }
     }
 
-    pub fn x(&self) -> f32 { self.vector.0 }
+    pub fn x(&self) -> f64 { self.vector.0 }
 
-    pub fn y(&self) -> f32 { self.vector.1 }
+    pub fn y(&self) -> f64 { self.vector.1 }
 
-    pub fn z(&self) -> f32 { self.vector.2 }
+    pub fn z(&self) -> f64 { self.vector.2 }
 
     /// Returns the length of the vector
     /// # Examples
@@ -44,7 +44,7 @@ impl Vec3d {
     /// let vec = Vec3d::new(1.0, 2.0, 3.0);
     /// assert_eq!(vec.length(), 3.7416574);
     /// ```
-    pub fn length(&self) -> f32 {
+    pub fn length(&self) -> f64 {
         self.length_squared().sqrt()
     }
 
@@ -55,7 +55,7 @@ impl Vec3d {
     /// let vec = Vec3d::new(1.0, 2.0, 3.0);
     /// assert_eq!(vec.length_squared(), 14.0);
     /// ```
-    pub fn length_squared(&self) -> f32 {
+    pub fn length_squared(&self) -> f64 {
         self.x().powi(2) + self.y().powi(2) + self.z().powi(2)
     }
 
@@ -96,7 +96,7 @@ impl std::fmt::Display for Vec3d {
 /// let result = dot(&vec, &vec2);
 /// assert_eq!(result, 32.0);
 /// ```
-pub fn dot(v1: &Vec3d, v2: &Vec3d) -> f32 {
+pub fn dot(v1: &Vec3d, v2: &Vec3d) -> f64 {
     v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z()
 }
 
@@ -110,7 +110,7 @@ pub fn dot(v1: &Vec3d, v2: &Vec3d) -> f32 {
 /// let result = distance(&vec, &vec2);
 /// assert_eq!(result, 5.0);
 /// ```
-pub fn distance<'a>(v1: &'a Vec3d, v2: &'a Vec3d) -> f32 {
+pub fn distance<'a>(v1: &'a Vec3d, v2: &'a Vec3d) -> f64 {
     (*v1 - *v2).length()
 }
 
@@ -182,10 +182,10 @@ impl Add<Vec3d> for Vec3d {
     }
 }
 
-impl Add<f32> for Vec3d {
+impl Add<f64> for Vec3d {
     type Output = Self;
 
-    fn add(self, rhs: f32) -> Self::Output {
+    fn add(self, rhs: f64) -> Self::Output {
         Self {
             vector: (self.x() + rhs, self.y() + rhs, self.z() + rhs)
         }
@@ -216,8 +216,8 @@ impl AddAssign<Vec3d> for Vec3d {
     }
 }
 
-impl AddAssign<f32> for Vec3d {
-    fn add_assign(&mut self, rhs: f32) {
+impl AddAssign<f64> for Vec3d {
+    fn add_assign(&mut self, rhs: f64) {
         self.vector.0 += rhs;
         self.vector.1 += rhs;
         self.vector.2 += rhs;
@@ -253,9 +253,9 @@ impl Sub<Vec3d> for Vec3d {
     }
 }
 
-impl Sub<f32> for Vec3d {
+impl Sub<f64> for Vec3d {
     type Output = Self;
-    fn sub(self, rhs: f32) -> Self::Output {
+    fn sub(self, rhs: f64) -> Self::Output {
         Self {
             vector: (self.x() - rhs, self.y() - rhs, self.z() - rhs)
         }
@@ -285,8 +285,8 @@ impl SubAssign<Vec3d> for Vec3d {
     }
 }
 
-impl SubAssign<f32> for Vec3d {
-    fn sub_assign(&mut self, rhs: f32) {
+impl SubAssign<f64> for Vec3d {
+    fn sub_assign(&mut self, rhs: f64) {
         self.vector.0 -= rhs;
         self.vector.1 -= rhs;
         self.vector.2 -= rhs;
@@ -318,10 +318,10 @@ impl Mul<Vec3d> for Vec3d {
     }
 }
 
-impl Mul<f32> for Vec3d {
+impl Mul<f64> for Vec3d {
     type Output = Self;
 
-    fn mul(self, rhs: f32) -> Self::Output {
+    fn mul(self, rhs: f64) -> Self::Output {
         Self {
             vector: (self.x() * rhs, self.y() * rhs, self.z() * rhs)
         }
@@ -352,8 +352,8 @@ impl MulAssign<Vec3d> for Vec3d {
     }
 }
 
-impl MulAssign<f32> for Vec3d {
-    fn mul_assign(&mut self, rhs: f32) {
+impl MulAssign<f64> for Vec3d {
+    fn mul_assign(&mut self, rhs: f64) {
         self.vector.0 *= rhs;
         self.vector.1 *= rhs;
         self.vector.2 *= rhs;
@@ -386,10 +386,10 @@ impl Div<Vec3d> for Vec3d {
     }
 }
 
-impl Div<f32> for Vec3d {
+impl Div<f64> for Vec3d {
     type Output = Self;
 
-    fn div(self, rhs: f32) -> Self::Output {
+    fn div(self, rhs: f64) -> Self::Output {
         return self * (1.0 / rhs);
     }
 }
@@ -418,8 +418,8 @@ impl DivAssign<Vec3d> for Vec3d {
     }
 }
 
-impl DivAssign<f32> for Vec3d {
-    fn div_assign(&mut self, rhs: f32) {
+impl DivAssign<f64> for Vec3d {
+    fn div_assign(&mut self, rhs: f64) {
         *self *= 1.0 / rhs;
     }
 }
@@ -513,7 +513,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_add_f32() {
+    fn test_vec3d_add_f64() {
         let vec = Vec3d::new(1.0, 2.0, 3.0);
         let result = vec + 2.0;
         assert_eq!(result, Vec3d::new(3.0, 4.0, 5.0));
@@ -528,7 +528,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_add_assign_f32() {
+    fn test_vec3d_add_assign_f64() {
         let mut vec = Vec3d::new(1.0, 2.0, 3.0);
         vec += 2.0;
         assert_eq!(vec, Vec3d::new(3.0, 4.0, 5.0));
@@ -543,7 +543,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_sub_f32() {
+    fn test_vec3d_sub_f64() {
         let vec = Vec3d::new(1.0, 2.0, 3.0);
         let result = vec - 2.0;
         assert_eq!(result, Vec3d::new(-1.0, 0.0, 1.0));
@@ -558,7 +558,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_sub_assign_f32() {
+    fn test_vec3d_sub_assign_f64() {
         let mut vec = Vec3d::new(1.0, 2.0, 3.0);
         vec -= 2.0;
         assert_eq!(vec, Vec3d::new(-1.0, 0.0, 1.0));
@@ -573,7 +573,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_mul_f32() {
+    fn test_vec3d_mul_f64() {
         let vec = Vec3d::new(1.0, 2.0, 3.0);
         let result = vec * 2.0;
         assert_eq!(result, Vec3d::new(2.0, 4.0, 6.0));
@@ -588,7 +588,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_mul_assign_f32() {
+    fn test_vec3d_mul_assign_f64() {
         let mut vec = Vec3d::new(1.0, 2.0, 3.0);
         vec *= 2.0;
         assert_eq!(vec, Vec3d::new(2.0, 4.0, 6.0));
@@ -603,7 +603,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_div_f32() {
+    fn test_vec3d_div_f64() {
         let vec = Vec3d::new(1.0, 2.0, 3.0);
         let result = vec / 2.0;
         assert_eq!(result, Vec3d::new(0.5, 1.0, 1.5));
@@ -618,7 +618,7 @@ mod vec3d_tests {
     }
 
     #[test]
-    fn test_vec3d_div_assign_f32() {
+    fn test_vec3d_div_assign_f64() {
         let mut vec = Vec3d::new(1.0, 2.0, 3.0);
         vec /= 2.0;
         assert_eq!(vec, Vec3d::new(0.5, 1.0, 1.5));
