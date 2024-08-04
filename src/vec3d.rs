c68609017aecd8157d863a98150bb4cb5ed297ba
GdoongMathew <gdoongmathew@gmail.com>
2024-08-04 16:57:56 +0800

    docs: add documentation on vec3d module.

--- src/vec3d.rs
+++ src/vec3d.rs
@@ -7,6 +7,23 @@ pub struct Vec3d {
     vector: (f32, f32, f32),
 }
 
+/// Implementation of ``Vec3d``
+///
+/// This is a struct that represents a 3D vector with x, y, and z components.
+/// Normally, this struct is used to represent points in 3D space, but it can also be used to
+/// represent colors. In this particular library, it's used to represent vectors or points in
+/// 3D space, and can be used for various calculations.
+/// Most of this struct's implementation follows the same pattern in the website
+/// [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html)
+///
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// assert_eq!(vec.x(), 1.0);
+/// assert_eq!(vec.y(), 2.0);
+/// assert_eq!(vec.z(), 3.0);
+/// ```
 impl Vec3d {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self {
@@ -20,14 +37,36 @@ impl Vec3d {
 
     pub fn z(&self) -> f32 { self.vector.2 }
 
+    /// Returns the length of the vector
+    /// # Examples
+    /// ```
+    /// use ray_tracing::vec3d::Vec3d;
+    /// let vec = Vec3d::new(1.0, 2.0, 3.0);
+    /// assert_eq!(vec.length(), 3.7416574);
+    /// ```
     pub fn length(&self) -> f32 {
         self.length_squared().sqrt()
     }
 
+    /// Returns the squared length of the vector
+    /// # Examples
+    /// ```
+    /// use ray_tracing::vec3d::Vec3d;
+    /// let vec = Vec3d::new(1.0, 2.0, 3.0);
+    /// assert_eq!(vec.length_squared(), 14.0);
+    /// ```
     pub fn length_squared(&self) -> f32 {
         self.x().powi(2) + self.y().powi(2) + self.z().powi(2)
     }
 
+    /// Returns the unit vector of the vector
+    /// # Examples
+    /// ```
+    /// use ray_tracing::vec3d::Vec3d;
+    /// let vec = Vec3d::new(1.0, 2.0, 3.0);
+    /// let result = vec.unit_vector();
+    /// assert_eq!(result, Vec3d::new(0.26726124, 0.5345225, 0.8017837));
+    /// ```
     pub fn unit_vector(&self) -> Self {
         *self / self.length()
     }
@@ -47,14 +86,44 @@ impl std::fmt::Display for Vec3d {
     }
 }
 
+
+/// The dot product of two Vec3d vectors
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::{Vec3d, dot};
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// let result = dot(&vec, &vec2);
+/// assert_eq!(result, 32.0);
+/// ```
 pub fn dot(v1: &Vec3d, v2: &Vec3d) -> f32 {
     v1.x() * v2.x() + v1.y() * v2.y() + v1.z() * v2.z()
 }
 
+
+/// The distance between two vec3d vectors
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::{Vec3d, distance};
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 6.0, 3.0);
+/// let result = distance(&vec, &vec2);
+/// assert_eq!(result, 5.0);
+/// ```
 pub fn distance<'a>(v1: &'a Vec3d, v2: &'a Vec3d) -> f32 {
     (*v1 - *v2).length()
 }
 
+
+/// The cross product of two Vec3d vectors
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::{Vec3d, cross};
+/// let vec = Vec3d::new(1.0, 0.0, 0.0);
+/// let vec2 = Vec3d::new(0.0, 1.0, 0.0);
+/// let result = cross(&vec, &vec2);
+/// assert_eq!(result, Vec3d::new(0.0, 0.0, 1.0));
+/// ```
 pub fn cross(v1: &Vec3d, v2: &Vec3d) -> Vec3d {
     Vec3d::new(
         v1.y() * v2.z() - v1.z() * v2.y(),
@@ -67,7 +136,15 @@ pub fn cross(v1: &Vec3d, v2: &Vec3d) -> Vec3d {
 impl Neg for Vec3d {
     type Output = Self;
 
-        fn neg(self) -> Self::Output {
+    /// Returns the negation of the vector
+    /// # Examples
+    /// ```
+    /// use ray_tracing::vec3d::Vec3d;
+    /// let vec = Vec3d::new(1.0, 2.0, 3.0);
+    /// let result = -vec;
+    /// assert_eq!(result, Vec3d::new(-1.0, -2.0, -3.0));
+    /// ```
+    fn neg(self) -> Self::Output {
             Self {
                 vector: (-self.x(), -self.y(), -self.z())
             }
@@ -76,6 +153,21 @@ impl Neg for Vec3d {
 }
 
 
+/// Addition overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// let result = vec + vec2;
+/// assert_eq!(result, Vec3d::new(5.0, 7.0, 9.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let result = vec + 2.0;
+/// assert_eq!(result, Vec3d::new(3.0, 4.0, 5.0));
+/// ```
 impl Add<Vec3d> for Vec3d {
     type Output = Self;
 
@@ -100,6 +192,22 @@ impl Add<f32> for Vec3d {
     }
 }
 
+
+/// AddAssign overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// vec += vec2;
+/// assert_eq!(vec, Vec3d::new(5.0, 7.0, 9.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// vec += 2.0;
+/// assert_eq!(vec, Vec3d::new(3.0, 4.0, 5.0));
+/// ```
 impl AddAssign<Vec3d> for Vec3d {
     fn add_assign(&mut self, rhs: Self) {
         self.vector.0 += rhs.x();
@@ -107,7 +215,6 @@ impl AddAssign<Vec3d> for Vec3d {
         self.vector.2 += rhs.z();
     }
 }
-
 
 impl AddAssign<f32> for Vec3d {
     fn add_assign(&mut self, rhs: f32) {
@@ -118,6 +225,22 @@ impl AddAssign<f32> for Vec3d {
 }
 
 
+/// Subtraction overloading for Vec3d
+///
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// let result = vec - vec2;
+/// assert_eq!(result, Vec3d::new(-3.0, -3.0, -3.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let result = vec - 2.0;
+/// assert_eq!(result, Vec3d::new(-1.0, 0.0, 1.0));
+/// ```
 impl Sub<Vec3d> for Vec3d {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
@@ -139,6 +262,21 @@ impl Sub<f32> for Vec3d {
     }
 }
 
+/// SubAssign overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// vec -= vec2;
+/// assert_eq!(vec, Vec3d::new(-3.0, -3.0, -3.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// vec -= 2.0;
+/// assert_eq!(vec, Vec3d::new(-1.0, 0.0, 1.0));
+/// ```
 impl SubAssign<Vec3d> for Vec3d {
     fn sub_assign(&mut self, rhs: Self) {
         self.vector.0 -= rhs.x();
@@ -155,6 +293,22 @@ impl SubAssign<f32> for Vec3d {
     }
 }
 
+
+/// Multiplication overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// let result = vec * vec2;
+/// assert_eq!(result, Vec3d::new(4.0, 10.0, 18.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let result = vec * 2.0;
+/// assert_eq!(result, Vec3d::new(2.0, 4.0, 6.0));
+/// ```
 impl Mul<Vec3d> for Vec3d {
     type Output = Self;
     fn mul(self, rhs: Self) -> Self::Output {
@@ -175,6 +329,21 @@ impl Mul<f32> for Vec3d {
 }
 
 
+/// MulAssign overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// vec *= vec2;
+/// assert_eq!(vec, Vec3d::new(4.0, 10.0, 18.0));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// vec *= 2.0;
+/// assert_eq!(vec, Vec3d::new(2.0, 4.0, 6.0));
+/// ```
 impl MulAssign<Vec3d> for Vec3d {
     fn mul_assign(&mut self, rhs: Self) {
         self.vector.0 *= rhs.x();
@@ -191,6 +360,22 @@ impl MulAssign<f32> for Vec3d {
     }
 }
 
+
+/// Division overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// let result = vec / vec2;
+/// assert_eq!(result, Vec3d::new(0.25, 0.4, 0.5));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let result = vec / 2.0;
+/// assert_eq!(result, Vec3d::new(0.5, 1.0, 1.5));
+/// ```
 impl Div<Vec3d> for Vec3d {
     type Output = Self;
     fn div(self, rhs: Self) -> Self::Output {
@@ -209,6 +394,22 @@ impl Div<f32> for Vec3d {
     }
 }
 
+
+/// DivAssign overloading for Vec3d
+/// # Examples
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// let vec2 = Vec3d::new(4.0, 5.0, 6.0);
+/// vec /= vec2;
+/// assert_eq!(vec, Vec3d::new(0.25, 0.4, 0.5));
+/// ```
+/// ```
+/// use ray_tracing::vec3d::Vec3d;
+/// let mut vec = Vec3d::new(1.0, 2.0, 3.0);
+/// vec /= 2.0;
+/// assert_eq!(vec, Vec3d::new(0.5, 1.0, 1.5));
+/// ```
 impl DivAssign<Vec3d> for Vec3d {
     fn div_assign(&mut self, rhs: Self) {
         self.vector.0 /= rhs.x();
