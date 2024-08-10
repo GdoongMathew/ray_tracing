813b8f7e5cc0bb3d263b07bd0cef57be0e407473
GdoongMathew <gdoongmathew@gmail.com>
2024-08-10 14:12:09 +0800

    remove: remove deprecated `ray_color` function in main as there's an identical impl in Camera module.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,9 @@
 use ray_tracing::vec3d::Vec3d;
-use ray_tracing::ray::{Interval, Ray};
 use ray_tracing::camera::Camera;
-use ray_tracing::object::{HitRecord, Sphere, Hittable, HittableVec};
+use ray_tracing::object::{Sphere, HittableVec};
 use ray_tracing::image::write_image;
 
 
-fn ray_color<H: Hittable>(ray: &Ray, world: &H) -> Vec3d {
-
-    let mut hit_record = HitRecord::new();
-    if world.hit(ray, &Interval{min: 0.0, max: f64::INFINITY}, &mut hit_record) {
-        return (hit_record.normal + Vec3d::new(1.0, 1.0, 1.0)) * 0.5;
-    }
-
-    let unit_direction = ray.direction.unit_vector();
-    let a = 0.5 * (unit_direction.y() + 1.0);
-    Vec3d::new(1.0, 1.0, 1.0) * (1.0 - a) + Vec3d::new(0.5, 0.7, 1.0) * a
-}
 
 fn main() {
     let camera = Camera::new(
