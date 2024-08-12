807c160520235c5ed31a1288f56b29423282252b
GdoongMathew <gdoongmathew@gmail.com>
2024-08-12 08:26:04 +0800

    feat: add Dielectric sphere in main.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use ray_tracing::vec3d::Vec3d;
 use ray_tracing::camera::Camera;
 use ray_tracing::object::{Sphere, HittableVec};
 use ray_tracing::image::write_image;
-use ray_tracing::object::material::{Material, Lambertian, Metal};
+use ray_tracing::object::material::{Material, Lambertian, Metal, Dielectric};
 
 
 
@@ -21,7 +21,8 @@ fn main() {
 
     let ground = Material::Lambertian(Lambertian::new(Vec3d::new(0.8, 0.8, 0.0)));
     let center = Material::Lambertian(Lambertian::new(Vec3d::new(0.1, 0.2, 0.5)));
-    let material_left = Material::Metal(Metal::new(Vec3d::new(0.8, 0.8, 0.8), 0.3));
+    let material_left = Material::Dielectric(Dielectric::new(1.0));
+    // let material_left = Material::Metal(Metal::new(Vec3d::new(0.8, 0.8, 0.8), 0.3));
     let material_right = Material::Metal(Metal::new(Vec3d::new(0.8, 0.6, 0.2), 1.0));
 
     world.add(Box::new(Sphere::new(Vec3d::new(0.0, -100.5, -1.0), 100.0, ground)));
